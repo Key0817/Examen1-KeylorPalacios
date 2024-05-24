63e3801d3bd273d62f0f9a205928dd46f833304d
Key0817 <keylorpalacios85@gmail.com>
2024-05-24 16:28:42 -0600

    Incorporación de un mailto en el botón (Contact Me)

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -13,7 +13,8 @@ export default function about(){
                     <p>Developer with superior Front-End skill, I have created multi-page layouts. 
                         I also have basic knowledge of programming..</p>
 
-                    <a className="btnp btn-about" href="https://www.linkedin.com/in/palacios-k/" target="_blank">Contact Me</a>
+                    <a className="btnp btn-about" href="mailto:keylorpalacios85@gmail.com?subject=Consultar%20servicio&body=Hola%20Keylor,
+                    %0AQuisiera%20saber%20m%C3%A1s%20sobre%20tus%20servicios"  >Contact Me</a>
 
                 </div>
             </main>
